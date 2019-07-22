b39a60896659262854f1640e0b2bebc15268f06e
waka <liangnida@163.com>
2019-07-22 17:41:18 +0800

    crf

--- baseline_2/extract.py
+++ baseline_2/extract.py
@@ -113,12 +113,12 @@ subject_model = SubjectModel.from_pretrained(pretrained_model_name_or_path=bert_
 
 subject_model.to(device)
 
-n_gpu = torch.cuda.device_count()
-if n_gpu > 1:
-    torch.cuda.manual_seed_all(42)
-
-    logger.info(f'let us use {n_gpu} gpu')
-    subject_model = torch.nn.DataParallel(subject_model)
+# n_gpu = torch.cuda.device_count()
+# if n_gpu > 1:
+#     torch.cuda.manual_seed_all(42)
+#
+#     logger.info(f'let us use {n_gpu} gpu')
+#     subject_model = torch.nn.DataParallel(subject_model)
 
 
 # optim
