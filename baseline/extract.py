9ee921d59b63b4e698810addcc52204b8b2912e6
waka <liangnida@163.com>
2019-07-25 18:35:51 +0800

    device

--- baseline/extract.py
+++ baseline/extract.py
@@ -25,7 +25,7 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-train_data = json.load((Path(data_dir)/'train_dev_train0724_filter_test.json.json').open())
+train_data = json.load((Path(data_dir)/'train_dev_train0724_filter_test.json').open())
 train_data  = train_data * 10
 dev_data = json.load((Path(data_dir)/'test_0724.json').open())
 
